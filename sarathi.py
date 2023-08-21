ae34d5fb0f731012c08ede16e39f92137bab5722
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-08-21 12:59:03 +0530

    Elif and Nested if Task

---
+++ sarathi.py
@@ -0,0 +1,101 @@
+##Task 7
+##num1=int(input("enter the value of num1"))
+##num2=int(input("enter the value of num2"))
+##num3=int(input("enter the value of num3"))
+##if(num1>num2)and(num1>num3):
+##    print("num1 is largest number")
+##elif(num2>num3):
+##    print("num2 is largest number")
+##Task3
+##b=int(input("enter the cost price of a bike"))
+##if(b>100000):
+##    print("the road tax is 15%")
+##elif(b>50000)and(b<=100000):
+##    print("the road tax is 10%")
+##elif(b<=50000):
+##    print("the road tax is 5%")
+##else:
+##    print("the road tax is not paid")
+##Task2
+##p=int(input("enter the percentage mark"))
+##if(p>=35):
+##    if(p>90)and(p==100):
+##        print("A grade")
+##    elif(p>80)and(p<=90):
+##        print("B grade")
+##    elif(p>=60)and(p<=80):
+##        print("C grade")
+##    elif(p<60):
+##        print("D grade")
+##else:
+##    print("you are fail")
+##Task 4:
+##num=int(input("enter the value of number"))
+##if(num==1)and(num!=8):
+##    print("sunday")
+##elif(num==2):
+##    print("Monday")
+##elif(num==3):
+##    print("Tuesday")
+##elif(num==4):
+##    print("Wednesday")
+##elif(num==5):
+##    print("Thursday")
+##elif(num==6):
+##    print("Friday")
+##elif(num==7):
+##    print("saturday")
+##else:
+##    print("Not define a days")
+##Task 5:
+##num=int(input("enter the value of number"))
+##if(num==1)and(num!=13):
+##    print("January and number of days is 31")
+##elif(num==2):
+##    print("February and number of days is 28")
+##elif(num==3):
+##    print("March and number of days is 31")
+##elif(num==4):
+##    print("April and number of days is 30")
+##elif(num==5):
+##    print("May and number of days is 31")
+##elif(num==6):
+##    print("June and number of days is 30")
+##elif(num==7):
+##    print("July and number of days is 31")
+##elif(num==8):
+##    print("August and number of days is 31")
+##elif(num==9):
+##    print("september and number of days is 30")
+##elif(num==10):
+##    print("october and number of days is 31")
+##elif(num==11):
+##    print("November and number of days is 30")
+##elif(num==12):
+##    print("December and number of days is 31")
+##else:
+##    print("Not define as month and date")
+
+    
+    
+    
+    
+        
+
+    
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
