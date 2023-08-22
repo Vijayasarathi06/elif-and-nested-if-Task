f262e13719f6913f2dca0837484fa76fce9477f9
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-08-22 12:40:06 +0530

    Add files via upload

---
+++ sarathi python task.py
@@ -0,0 +1,100 @@
+
+##task 1
+##unit=int(input("enter the electricity bill"))
+##if(unit>100):
+##    if(unit>100)and(unit<=200):
+##        amount1=(unit-100)*5
+##        print("total amount of elelctricity bill",amount1)
+##    elif(unit>200):
+##        amount2=(unit-100)*10
+##        print("total amount of elelctricity bill",amount2)
+##else:
+##    print("u have no charges")
+##Task 6
+##E=int(input("enter the value of equilateral triangle"))
+##I=int(input("enter the value of isosceles triangle"))
+##S=int(input("enter the value of scalene triangle"))
+##if(E==I)and(I==S):
+##    print("it is a equilateral triangle")
+##elif(E==I)or(E==S)or(I==S):
+##    print("it is a isosceles triangle")
+##elif(E!=I)and(E!=S):
+##    print("it is a scalene triangle")
+##Task 8
+##year=int(input("enter the year"))
+##if(year%4==0):
+##    if(year%100==0):
+##        if(year%400==0):
+##            print("ti is a leap year")
+##        else:
+##            print("it is not a leap year")
+##    else:
+##        print("it is leap year")
+##else:
+##    print("it is not a leap year")
+##Task9
+##num1=float(input("enter the value of num1"))
+##num2=float(input("enter the value of num2"))
+##operator=(input("enter the operator symbols(+,-,*,/):"))
+##if(operator=="+"):
+##    print("total is",num1+num2)
+##elif(operator=="-"):
+##    print("total is",num1-numn2)
+##elif(operator=="*"):
+##    print("total is",num1*num2)
+##elif(operator=="/"):
+##    print("total is",num1/num2)
+start=int(input("enter the start value"))
+stop=int(input("enter the stop vlaue"))
+table=int(input("enter the table"))
+a=5
+b=10
+c=15
+d=20
+e=25
+f=30
+g=35
+h=40
+i=45
+j=50
+for i in range(start,stop,table):
+    print("1*5=",a)
+
+
+
+
+
+
+
+
+for i in range(start,stop,table):
+    print("1*5=")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
